d810af244e6feac01e478ac94e242fd715b4213c
Your Name <matoufdanslezoo@gmail.com>
2025-02-26 11:43:09 +0100

    add : orderAPI

--- src/Entity/Order.php
+++ src/Entity/Order.php
@@ -2,35 +2,79 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Metadata\ApiResource;
+use ApiPlatform\Metadata\Get;
+use ApiPlatform\Metadata\GetCollection;
+use ApiPlatform\Metadata\Patch;
+use ApiPlatform\Metadata\Post;
+use ApiPlatform\Metadata\Delete;
+use App\DataPersister\BookDataPersister;
+use App\Repository\BookRepository;
 use App\Repository\OrderRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Serializer\Annotation\Groups;
 
 #[ORM\Entity(repositoryClass: OrderRepository::class)]
 #[ORM\Table(name: '`order`')]
+#[ApiResource(
+    operations: [
+        new Get(
+            normalizationContext: ['groups' => ['order:read']],
+            security: "is_granted('PUBLIC_ACCESS')"
+        ),
+        new GetCollection(
+            normalizationContext: ['groups' => ['order:read']],
+            security: "is_granted('PUBLIC_ACCESS')"
+        ),
+        new Post(
+            denormalizationContext: ['groups' => ['order:write']],
+            security: "is_granted('ROLE_USER')",
+            processor: BookDataPersister::class,
+            securityMessage: "Seuls les utilisateurs connectés peuvent créer des livres"
+        ),
+        new Patch(
+            denormalizationContext: ['groups' => ['order:write']],
+            security: "is_granted('ORDER_EDIT', object)",
+            securityMessage: "Vous ne pouvez modifier que vos propres livres"
+        ),
+        new Delete(
+            security: "is_granted('ORDER_DELETE', object)",
+            securityMessage: "Vous ne pouvez supprimer que vos propres livres"
+        ),
+    ]
+)]
 class Order
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
+    #[Groups(['order:read'])]
     private ?int $id = null;
 
     #[ORM\OneToOne(inversedBy: 'purchase', cascade: ['persist', 'remove'])]
     #[ORM\JoinColumn(nullable: false)]
+    #[Groups(['order:read', 'order:write'])]
     private ?User $buyer = null;
 
     #[ORM\OneToOne(inversedBy: 'sale', cascade: ['persist', 'remove'])]
     #[ORM\JoinColumn(nullable: false)]
+    #[Groups(['order:read', 'order:write'])]
     private ?User $seller = null;
 
     #[ORM\OneToOne(inversedBy: 'sale', cascade: ['persist', 'remove'])]
     #[ORM\JoinColumn(nullable: false)]
+    #[Groups(['order:read', 'order:write'])]
     private ?Book $book = null;
 
     #[ORM\Column]
+    #[Groups(['order:read', 'order:write'])]
     private ?\DateTimeImmutable $purchaseAt = null;
 
     #[ORM\OneToOne(inversedBy: 'sale', cascade: ['persist', 'remove'])]
     #[ORM\JoinColumn(nullable: false)]
+    #[Groups(['order:read', 'order:write'])]
     private ?OrderState $orderState = null;
 
     public function getId(): ?int
