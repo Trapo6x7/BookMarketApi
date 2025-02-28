8cb670100212ce8abb0db607c42b80852622dcc0
Your Name <matoufdanslezoo@gmail.com>
2025-02-28 11:16:51 +0100

    ADD : /ME

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -12,6 +12,8 @@ use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Serializer\Attribute\Groups;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
+use ApiPlatform\Metadata\Get;
+use App\State\Provider\MeProvider;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
 #[ORM\UniqueConstraint(name: 'UNIQ_IDENTIFIER_EMAIL', fields: ['email'])]
